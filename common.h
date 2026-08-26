31c0e3b0bfa4df4246062ef9f417af14ea33265c
R3D-ubuntu <ryuk24.gaming@gmail.com>
2026-08-26 19:54:25 +0200

    FUSE_DESTROY opcode & opcode string name & log verbosity levels & daemonization

---
+++ common.h
@@ -0,0 +1,3 @@
+
+extern const char* fuse_opcode_enum_str[];
+
--- fuse.c
+++ fuse.c
@@ -10,6 +10,8 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
+#include "common.h"
+
 
 #define FUSE_FILE "/dev/fuse"
 #define DEFAULT_MOUNT_POINT "/mnt/kim"
@@ -19,16 +21,18 @@
 #define PROTOCOL_VERSION_MINOR 39
 
 int fuse_fd = -1;
-bool mounted = false;
+enum {
+  LOG_QUIET = 0,
+  LOG_NORMAL = 1,
+  LOG_VERBOSE = 2
+} log_level;
 
 void cleanup(void) {
   if (fuse_fd != -1)
     if (close(fuse_fd) == -1)
-      perror("close");
+      if (log_level >= LOG_NORMAL) perror("close");
 
-  if (mounted)
-    if (umount2(DEFAULT_MOUNT_POINT, MNT_FORCE) == -1)
-      perror("umount");
+  // TODO: flush and save fs
 }
 
 void signal_handler(int sig) {
@@ -40,7 +44,7 @@ void signal_handler(int sig) {
 int main(void) {
   long PAGE_SIZE = sysconf(_SC_PAGESIZE);
   if (PAGE_SIZE == -1) {
-    perror("sysconf _SC_PAGESIZE");
+    if (log_level >= LOG_NORMAL) perror("sysconf _SC_PAGESIZE");
     exit(EXIT_FAILURE);
   }
 
@@ -49,19 +53,53 @@ int main(void) {
   signal(SIGTERM, signal_handler);
   signal(SIGHUP,  signal_handler);
 
+  bool daemonize = true;
+  log_level = LOG_VERBOSE;
+
+  // TODO: initialize fs from file
+  // lock backing storage file to prevent access
+
   fuse_fd = open(FUSE_FILE, O_RDWR);
   if (fuse_fd == -1) {
-    perror("open " FUSE_FILE);
+    if (log_level >= LOG_NORMAL) perror("open " FUSE_FILE);
     exit(EXIT_FAILURE);
   }
 
-
   char options[256];
   snprintf(options, sizeof(options), "fd=%d,rootmode=040777,user_id=%u,group_id=%u,default_permissions,allow_other,subtype=kim", fuse_fd, getuid(), getgid()); // TODO: fsname=NAME for the name of the backing storage file
-  mounted = true;
   if (mount("fuse", DEFAULT_MOUNT_POINT, "fuse", 0, options) == -1) {
-    perror("mount");
+    if (log_level >= LOG_NORMAL) perror("mount");
     exit(EXIT_FAILURE);
+  }
+
+  if (daemonize) {
+    pid_t pid = fork();
+    if (pid < 0) {
+      if (log_level >= LOG_NORMAL) perror("fork");
+      exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+      if (log_level >= LOG_VERBOSE) printf("[INFO] daemonized successfully. Daemon PID: %d\n", pid);
+      exit(EXIT_SUCCESS);
+    }
+
+    if (setsid() == -1) {
+      exit(EXIT_FAILURE);
+    }
+
+    if (chdir("/") == -1) {
+      exit(EXIT_FAILURE);
+    }
+
+    int null_fd = open("/dev/null", O_RDWR);
+    if (null_fd != -1) {
+      dup2(null_fd, STDIN_FILENO);
+      dup2(null_fd, STDOUT_FILENO);
+      dup2(null_fd, STDERR_FILENO);
+      if (null_fd > 2) {
+        close(null_fd);
+      }
+    }
   }
 
   unsigned long buf_size = (MAX_PAGES + 1) * (unsigned long) PAGE_SIZE;
@@ -72,19 +110,19 @@ int main(void) {
   while (running) {
     count = read(fuse_fd, buf, buf_size);
     if (count == -1) {
-      perror("read");
+      if (log_level >= LOG_NORMAL) perror("read");
       exit(EXIT_FAILURE);
     }
 
     struct fuse_in_header in_header = *(struct fuse_in_header*) buf;
     void* payload = &buf[sizeof(struct fuse_in_header)];
 
-    printf("[INFO] received op %u from the kernel\n", in_header.opcode);
+    if (log_level >= LOG_VERBOSE) printf("[INFO] received %s (%u) from the kernel\n", fuse_opcode_enum_str[in_header.opcode], in_header.opcode);
     if (in_header.opcode == FUSE_INIT) {
       struct fuse_init_in init_in = *(struct fuse_init_in*) payload;
-      printf("[INFO] kernel's FUSE protocol version is %u.%u (supported is %u.%u)\n", init_in.major, init_in.minor, PROTOCOL_VERSION_MAJOR, PROTOCOL_VERSION_MINOR);
+      if (log_level >= LOG_VERBOSE) printf("[INFO] kernel's FUSE protocol version is %u.%u (supported is %u.%u)\n", init_in.major, init_in.minor, PROTOCOL_VERSION_MAJOR, PROTOCOL_VERSION_MINOR);
       if (init_in.major < PROTOCOL_VERSION_MAJOR) {
-        fprintf(stderr, "error: kernel's FUSE protocol version is too old (%u.%u < %u.%u)", init_in.major, init_in.minor, PROTOCOL_VERSION_MAJOR, PROTOCOL_VERSION_MINOR);
+        if (log_level >= LOG_NORMAL) fprintf(stderr, "error: kernel's FUSE protocol version is too old (%u.%u < %u.%u)", init_in.major, init_in.minor, PROTOCOL_VERSION_MAJOR, PROTOCOL_VERSION_MINOR);
         exit(EXIT_FAILURE);
       } else if (init_in.major > PROTOCOL_VERSION_MAJOR) {
         struct fuse_out_header out_header = (struct fuse_out_header) {
@@ -99,7 +137,7 @@ int main(void) {
         };
 
         if (writev(fuse_fd, iov, 2) == -1) {
-          perror("writev");
+          if (log_level >= LOG_NORMAL) perror("writev");
           exit(EXIT_FAILURE);
         }
       } else {
@@ -127,7 +165,7 @@ int main(void) {
         };
 
         if (writev(fuse_fd, iov, 2) == -1) {
-          perror("writev");
+          if (log_level >= LOG_NORMAL) perror("writev");
           exit(EXIT_FAILURE);
         }
 
@@ -146,26 +184,46 @@ int main(void) {
       };
 
       if (writev(fuse_fd, iov, 1) == -1) {
-        perror("writev");
+        if (log_level >= LOG_NORMAL) perror("writev");
         exit(EXIT_FAILURE);
       }
     }
     switch (in_header.opcode) {
+      case FUSE_DESTROY:
+        {
+          struct fuse_out_header out_header = (struct fuse_out_header) {
+            .len = sizeof(struct fuse_out_header),
+            .error = 0,
+            .unique = in_header.unique
+          };
+
+          struct iovec iov[] = {
+            { &out_header, sizeof(struct fuse_out_header) }
+          };
+
+          if (writev(fuse_fd, iov, 1) == -1) {
+            if (log_level >= LOG_NORMAL) perror("writev (FUSE_DESTROY)");
+          }
+
+          running = false;
+          break;
+        }
       default:
-        ;
-        struct fuse_out_header out_header = (struct fuse_out_header) {
-          .len = sizeof(struct fuse_out_header),
-          .error = -EOPNOTSUPP,
-          .unique = in_header.unique
-        };
+        {
+          struct fuse_out_header out_header = (struct fuse_out_header) {
+            .len = sizeof(struct fuse_out_header),
+            .error = -EOPNOTSUPP,
+            .unique = in_header.unique
+          };
 
-        struct iovec iov[] = {
-          { &out_header, sizeof(struct fuse_out_header) }
-        };
+          struct iovec iov[] = {
+            { &out_header, sizeof(struct fuse_out_header) }
+          };
 
-        if (writev(fuse_fd, iov, 1) == -1) {
-          perror("writev");
-          exit(EXIT_FAILURE);
+          if (writev(fuse_fd, iov, 1) == -1) {
+            if (log_level >= LOG_NORMAL) perror("writev");
+            exit(EXIT_FAILURE);
+          }
         }
     }
   }
